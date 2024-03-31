d59fc942b64ca8b7283eda9972ca51ffe173035a
callanroff123 <callanroff@gmail.com>
2024-03-31 12:42:36 +1100

    Fixed eventbrite scrape (they seem to update their html alot)

--- src/event_extraction/eventbrite.py
+++ src/event_extraction/eventbrite.py
@@ -36,8 +36,11 @@ options.add_argument("start-maximized")
 options.add_argument("--disable-notifications")
 venues_eventbrite = [
     i for i in venues if i in [
+        "Sub Club Melbourne",
+        "The Retreat Hotel",
         "280 Sydney Rd",
-        "Sub Club"
+        "Sub Club",
+        "The Retreat"
     ]
 ]
 
@@ -46,7 +49,6 @@ def get_events_eventbrite():
     '''
         Gets events from eventbrite.com.au
     '''
-    driver = webdriver.Chrome()
     time.sleep(1)
     df_final = pd.DataFrame({
         "Title": [""],
@@ -57,13 +59,14 @@ def get_events_eventbrite():
     })
     for venue in venues_eventbrite:
         try:
+            driver = webdriver.Chrome()
             driver.get(f"https://www.eventbrite.com.au/d/australia--melbourne/{venue.replace(' ', '-')}/")
             time.sleep(1)
             soup = BeautifulSoup(
                 driver.page_source, "html"
             )
             postings = soup.find_all(
-                "li", {"class": "search-main-content__events-list-item search-main-content__events-list-item__mobile"})
+                "section", {"class": "event-card-details"})
             df = pd.DataFrame({
                 "Title": [""],
                 "Date": [""],
@@ -73,12 +76,12 @@ def get_events_eventbrite():
             })
             for post in postings:
                 title = post.find(
-                    "h2", {"class": "Typography_root__4bejd #3a3247 Typography_body-lg__4bejd event-card__clamp-line--two Typography_align-match-parent__4bejd"}).text.strip()
+                    "h2").text.strip()
                 ven = venue.split(",", 1)[0]
                 ven1 = post.find_all(
-                    "p", {"class": "Typography_root__4bejd #585163 Typography_body-md__4bejd event-card__clamp-line--one Typography_align-match-parent__4bejd"})[0].text.strip()
+                    "p")[-3:][1].text.strip()
                 date = post.find_all(
-                    "p", {"class": "Typography_root__4bejd #585163 Typography_body-md__4bejd event-card__clamp-line--one Typography_align-match-parent__4bejd"})[1].text.strip()
+                    "p")[-3:][0].text.strip()
                 link = post.find(
                     "a", {"class": "event-card-link"}).get("href")
                 df = pd.concat(
@@ -93,6 +96,7 @@ def get_events_eventbrite():
                 df = df.reset_index(drop=True)
             df_final = pd.concat([df_final, df], axis = 0).reset_index(drop = True)
             time.sleep(1)
+            driver.close()
         except:
             pass
     df_final = df_final[df_final["Title"] != ""].reset_index(drop=True)
